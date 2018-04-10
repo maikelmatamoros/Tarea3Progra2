db7d2c5763d2e5d52facd7544dd763519977d964
YerLeal <yeryazmin96@gmail.com>
2018-04-09 20:45:40 -0600

    Commit 3

--- tarea3progra2/Tarea3Progra2.java
+++ tarea3progra2/Tarea3Progra2.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tarea3progra2;
 
 import java.awt.Color;
@@ -11,41 +6,29 @@ import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-/**
- *
- * @author Nelson
- */
 public class Tarea3Progra2 extends JPanel {
 
     public Tarea3Progra2() {
         this.setPreferredSize(new Dimension(800, 600));
     } // constructor
 
+    private int numAleatorio(int n) {
+        return (int) (Math.random() * n) + 1;
+    }
+
     private void draw(Graphics g) {
-        // dibujar los ejes del plano cartesiano
-        g.setColor(Color.red);
-
-        g.setColor(Color.black);
-//        linearFunction(g, 110, 600, 100, 400);
-        for(int i=0;i<1000;i++){
-            int x0=(int) (Math.random() * 800 + 1);
-            int y0=(int) (Math.random() * 600 + 1);
-            int x1=(int) (Math.random() * 800 + 1);
-            int y1=(int) (Math.random() * 600 + 1);
-            linearFunction(g, x0, y0, x1, y1);
-            
-        }
-        
-
+        for (int i = 0; i < 1000; i++) {
+            g.setColor(new Color(numAleatorio(255), numAleatorio(255), numAleatorio(255)));
+            int x0 = (int) (Math.random() * 800 + 1);
+            int y0 = (int) (Math.random() * 600 + 1);
+            int x1 = (int) (Math.random() * 800 + 1);
+            int y1 = (int) (Math.random() * 600 + 1);
+            linearFunction(g, numAleatorio(800), numAleatorio(600), numAleatorio(800), numAleatorio(600));
+        } // for
     } // draw
 
-    // funcion lineal f(x) = m x + b
-    // x1 y x2 es el rango en el que se graficara la funcion
     private void linearFunction(Graphics g, double x0, double y0, double x1, double y1) {
-
         double dx = x1 - x0;
-        
-        //double punto;
         g.drawLine((int) x0, (int) y0, (int) x1, (int) y1);
         if (dx != 0) {
             double m = (y0 - y1) / (x0 - x1);
@@ -59,22 +42,17 @@ public class Tarea3Progra2 extends JPanel {
                 x0 += dx;
                 y0 = Math.round(m * x0 + b);
                 g.drawLine((int) x0, (int) y0, (int) x0, (int) y0);
-            }
-        }
-
+            } // while
+        } // if
     }// linearFunction
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        // se llama al meto draw
-        draw(g);
-
-    }
+        draw(g); // se llama al metodo draw
+    } // paintComponent
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!! XD");
-        
         JFrame window = new JFrame("Graphing Function");
         window.setContentPane(new Tarea3Progra2());
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -82,7 +60,7 @@ public class Tarea3Progra2 extends JPanel {
         window.setResizable(false);
         window.setLocation(149, 100);
         window.setVisible(true);
-    }
+    } // main
 
 } // fin clase
 
